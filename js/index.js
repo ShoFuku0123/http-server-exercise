bfc094f831a4f5e5a58cb38d33a7a5627eae8f75
Sho Fukuyama <sho.fukuyama0123@gmail.com>
2023-08-16 16:42:25 +0900

    update index -> js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+<script>
+    function clickHandler() {
+        const box = document.getElementById("box");
+        const p = document.createElement("p");
+        p.innerText = "Added by JS";
+        box.appendChild(p);
+    }
+</script>
